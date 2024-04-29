e574b57f889832bdd8ef796aa318b1fb727ee0cf
Billy Junaidi <billy.junaidi@gmail.com>
2024-04-29 16:21:53 +0800

    update create validator

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ SERVICE_NAME=galacticad-testnet
 INSTALLATION_DIR=$(dirname "$(realpath "$0")")
 CHAIN_ID='galactica_9302-1'
 GENESIS_URL="https://raw.githubusercontent.com/Galactica-corp/networks/main/galactica_9302-1/genesis.json"
-PEERS="c9993c738bec6a10cfb8bb30ac4e9ae6f8286a5b@galactica-testnet-peer.itrocket.net:11656,f3cd6b6ebf8376e17e630266348672517aca006a@46.4.5.45:27456,6b846b316d704d78f3f9ca46d86cebc5a22de8ae@161.97.111.249:26656,232050092a90b94b002f34a79de8a3859ed13ee7@5.104.108.184:26656,9990ab130eac92a2ed1c3d668e9a1c6e811e8f35@148.251.177.108:27456,c722e6dc5f762b0ef19be7f8cc8fd67cdf988946@49.12.96.14:26656,e926f2e20568e61646558a2b8fd4a4e46d77903f@141.95.110.124:26656,3afb7974589e431293a370d10f4dcdb73fa96e9b@157.90.158.222:26656,dc4ed6e614725dffc41874e762a1b1ce4cdc3304@161.97.74.20:46656,e38c22e44893e75e388f3bcea2a075124d75ccd3@89.110.89.244:26656,8949fb771f2859248bf8b315b6f2934107f1cf5a@168.119.241.1:26656"
+PEERS="cd3e3746b83a33c857a97be4986f16425e03b880@galactica-testnet-peer.cryptonode.id:25656,c9993c738bec6a10cfb8bb30ac4e9ae6f8286a5b@galactica-testnet-peer.itrocket.net:11656,f3cd6b6ebf8376e17e630266348672517aca006a@46.4.5.45:27456,6b846b316d704d78f3f9ca46d86cebc5a22de8ae@161.97.111.249:26656,232050092a90b94b002f34a79de8a3859ed13ee7@5.104.108.184:26656,9990ab130eac92a2ed1c3d668e9a1c6e811e8f35@148.251.177.108:27456,c722e6dc5f762b0ef19be7f8cc8fd67cdf988946@49.12.96.14:26656,e926f2e20568e61646558a2b8fd4a4e46d77903f@141.95.110.124:26656,3afb7974589e431293a370d10f4dcdb73fa96e9b@157.90.158.222:26656,dc4ed6e614725dffc41874e762a1b1ce4cdc3304@161.97.74.20:46656,e38c22e44893e75e388f3bcea2a075124d75ccd3@89.110.89.244:26656,8949fb771f2859248bf8b315b6f2934107f1cf5a@168.119.241.1:26656"
 RPC="https://rpc-reticulum.galactica.com:443"
 SEEDS="c722e6dc5f762b0ef19be7f8cc8fd67cdf988946@seed01-reticulum.galactica.com:26656,8949fb771f2859248bf8b315b6f2934107f1cf5a@seed02-reticulum.galactica.com:26656,3afb7974589e431293a370d10f4dcdb73fa96e9b@seed03-reticulum.galactica.com:26656"
 DENOM='agnet'
@@ -205,7 +205,7 @@ ${DAEMON_NAME} tx staking create-validator \\
   --identity="${INPUT_IDENTITY}" \\
   --website="${INPUT_WEBSITE}" \\
   --details="${INPUT_DETAILS}" \\
-  --security_contact="${INPUT_EMAIL}" \\
+  --security-contact="${INPUT_EMAIL}" \\
   --chain-id="$CHAIN_ID" \\
   --commission-rate="0.05" \\
   --commission-max-rate="0.2" \\
